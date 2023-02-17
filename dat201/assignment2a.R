a7eca2944e724908f5bb9090ed8a5f6993f2bf4c
Colin Bowers <colin.bowers@gmail.com>
2023-02-17 10:35:57 -0500

    Added a summary of results and comments.

--- dat201/assignment2a.R
+++ dat201/assignment2a.R
@@ -13,30 +13,27 @@
 # Create the sampling distribution of sample mean.
 # Provide necessary figures and comments
 
-
-#install.packages("countreg", repos="http://R-Forge.R-project.org")
-
+# Required package for the rzinbinom() function 
+# install.packages("countreg", repos="http://R-Forge.R-project.org")
 library(countreg)
-#set.seed(27012022)
-set.seed(123456)
 
+set.seed(123456)
 
 n = 30      # no. of observations per sample
 t = 10      # no. of trials within each observation
 p = 0.5     # probability of success for each trial
 N = 100000  # no. of samples
 
-
-# mu: the mean of the population
+# mu: the theoretical mean of the population
 mu = t * p
 
-# sigma: the std dev of the population 
+# sigma: the theoretical std dev of the population 
 sigma = sqrt(p*(1-p))
 
-
-
+# S: the sample data.  Placed into a matrix shaped N rows x n cols.
 S = matrix(rzinbinom(n*N, mu, t, pi=0), nrow=N)
 
+# S.means: a vector containing the mean of each row of S
 S.means = rowMeans(S)
 hist(S.means, prob=TRUE, xlab="Sample Mean")
 
@@ -44,10 +41,27 @@ hist(S.means, prob=TRUE, xlab="Sample Mean")
 xbar = mean(S.means)
 abline(v=xbar, col="red")
 
-#plot the theoretical normal distribution
+# plot the theoretical normal distribution
 xx = seq(min(S.means), max(S.means), by=0.01)
-
 lines(xx, dnorm(xx, mu, sigma), col="blue") 
 
+results = c(
+  "mu" = mu,
+  "xbar" = xbar,
+  "sigma" = sigma,
+  "s" = sd(S.means)
+)
+results
+
+# ------------------------ COMMENTS ------------------------
+#
+#          mu      xbar     sigma         s 
+#   5.0000000 5.0023307 0.5000000 0.5003995 
+#
+# The theoretical normal distribution follows the distribution of sample means 
+# quite closely. This implies the mean and std dev between both distributions 
+# should also be very close which is what our results show.  Since our sample 
+# size of 100,000 is large, this is in agreement with the Central Limit Theorem.
+#
 
 
