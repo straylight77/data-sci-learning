2b7ed5abde09e561ed737ce9773a23dc6e101175
Colin Bowers <colin.bowers@gmail.com>
2023-03-11 12:43:42 -0500

    Some cleanups.  Better answers (I think.)

--- dat201/assignment3.R
+++ dat201/assignment3.R
@@ -29,37 +29,16 @@ abline(fit, col="red")
 
 summary(fit)
 
-# can also calculate these manually (Devore p. 479)
-n = length(df$Age)
-S.xy = sum(df$Age * df$Length) - ( sum(df$Age) * sum(df$Length) / n)
-S.xx = sum(df$Age ^ 2) - (sum(df$Age)^2)/n
-B1.hat = S.xy / S.xx
-B0.hat = mean(df$Length) - B1.hat * mean(df$Age)
-
-# the final fitted linear equation as a function 
-f = function(x) {
-  y = 65.5272 + 30.3239 * x
-  return(y)
-}
-
-
-# (optional) Let's calulate and analyze the deviations (residuals)
-error = df$Length - f(df$Age)
-summary(error)
-sigma.hat = sum(df$Length - f(df$Age)) ^ 2 / (n-2)   # not working
-sd(error)  # same as "Residual standard error: 28.65 on 437 degrees of freedom"
-sum(error)   # should be close to 0 
-
 
 
 # ---------------------------------------------------------------------------
 # 2. What proportion of the variability in y(length) explained by the linear 
 #    relationship
 
-# Coefficient of Determination (Devore p. 485)
-R.sq = 0.8165    # from summary(fit)
-# Therefore, approximately 81.7% of the observed variations in fish length 
-# can be explained by this regression model.  
+# Coefficient of Determination R^2 -> from summary(fit)
+r.sq = 0.8165
+# This tells us that approximately 81.7% of the observed variations in fish 
+# length can be explained by this regression model.  
 
 
 
@@ -67,12 +46,12 @@ R.sq = 0.8165    # from summary(fit)
 # 3. What is the slope of the regression line. Does the true slope is equal 
 #    to 0? Provide your answer using the hypothesis testing.
 
-slope = b1 = fit$coefficients[2]    # slope = 30.3
+slope = b1 = fit$coefficients[2]           # 30.3
 
 #      null hypothesis H0:  b1 = 0
 #       alt hypothesis Ha:  b1 != 0
 
-p.value = summary(fit)$coefficients[2,4]
+p.value = summary(fit)$coefficients[2,4]   # <2e-16
 
 # CONCLUSION:
 # A very small value for p implies very strong evidence to reject H0 and 
@@ -89,18 +68,9 @@ tstat = b1 / se
 # 4. What measures the amount of variability in y(length) about the line at 
 #    a given value of x(age). Provide the numeric value from the R output.
 
-# Devore p. 486
-#https://medium.com/analytics-vidhya/the-measures-of-variation-of-a-linear-regression-model-e85f0258ab3f 
-
-# Regression Sum of Squares (SSR) - "Explained Variability"
-# SSR = SST - SSE = sum (predicted value for Y - mean value of y)^2
-SSR = sum(f(df$Age) - mean(df$Length))^2
-
-# Error Sum of Squares (SSE) - "Unexplained Variability"
-SSE = sum(df$Length - f(df$Age))^2
-
-# Total Sum of Squares (SST)
-SST = sum(df$Length - mean(df$Length))^2
+# Residual standard error -> from summary(fit)
+# Describes the deviation from the model that the observed data represents. 
+rse = 28.65
 
 
 
@@ -108,15 +78,40 @@ SST = sum(df$Length - mean(df$Length))^2
 # 5. Is there any relationship between the fish age and fish length?
 
 # coefficient of correlation
-r = sqrt(R.sq)                   # 0.9036039
+r = sqrt(r.sq)                   # 0.9036039
 corr = cor(df$Length, df$Age)    # 0.9035912
-
 
 # CONCLUSION:
 # The coefficient of correlation, r=0.904, is positive and it is very close to 
-# +1, which means that the fish length (y) and age (x) has a very strong 
+# +1, which means that the fish length (y) and age (x) have a very strong 
 # linear relationship.
 
 
+
+
+
+
+
+
+# can also calculate these manually (Devore p. 479)
+n = length(df$Age)
+S.xy = sum(df$Age * df$Length) - ( sum(df$Age) * sum(df$Length) / n)
+S.xx = sum(df$Age ^ 2) - (sum(df$Age)^2)/n
+B1.hat = S.xy / S.xx
+B0.hat = mean(df$Length) - B1.hat * mean(df$Age)
+
+# the final fitted linear equation as a function 
+f = function(x) {
+  y = 65.5272 + 30.3239 * x
+  return(y)
+}
+
+# (optional) Let's calculate and analyze the deviations (residuals)
+error = df$Length - f(df$Age)
+summary(error)  # matches summary(fit)
+sd(error)       # matches "Residual standard error: 28.65 on 437 degrees of freedom"
+sum(error)      # should be close to 0 
+
+sigma.hat = sum(df$Length - f(df$Age)) ^ 2 / (n-2)   # not working
 
 
